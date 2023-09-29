e2b088c20ad256cc7f86676ccabf0cba29c6d966
Lee Aspitarte <aspitarte@gmail.com>
2023-09-28 19:09:04 -0700

    initial setup experiment analysis 53x

---
+++ experiment/collect_data.py
@@ -0,0 +1,96 @@
+#%%
+
+from mhdpy.analysis.standard_import import *
+
+notebook_dir = os.getenv('EXPT_NB_DIR')
+
+import re
+
+regex = re.compile('^\d{4}-\d\d-\d\d$')
+
+
+dates = []
+for dir in os.listdir(notebook_dir):
+    m = regex.match(dir)
+    if m:
+        dates.append(dir)
+    
+dates
+# %%
+
+
+## Absem
+
+dss = []
+for date in dates:
+    fp = pjoin(notebook_dir, date, 'absem','proc_data', 'ds_alpha.cdf')
+    ds = xr.load_dataset(fp)
+    ds = ds[['alpha']]
+    dss.append(ds)
+
+
+#%%
+
+ds_absem = xr.concat(dss, 'time')
+
+ds_absem.to_netcdf(pjoin('proc_data','ds_absem.cdf'))
+#%%
+
+## MWS
+
+
+dss = []
+for date in dates:
+    fp = pjoin(notebook_dir, date, 'mws','proc_data', 'ds_lecroy_time.cdf')
+    ds = xr.load_dataset(fp)
+    ds = ds[['i', 'q']]
+    dss.append(ds)
+
+#%%
+
+ds_lecroy = xr.concat(dss, 'time')
+ds_lecroy.to_netcdf(pjoin('proc_data','ds_lecroy.cdf'))
+# %%
+
+
+from mhdpy.io import load_df_cuttimes, extract_cuttime_list
+
+dfs = []
+for date in dates:
+    fp = pjoin(notebook_dir, date, 'mws','cuttimes.csv')
+    df_cuttimes = load_df_cuttimes(fp).sort_values('Start Time').reset_index(drop=True)
+    df_cuttimes['date'] = date
+    dfs.append(df_cuttimes)
+
+df_cuttimes = pd.concat(dfs)
+
+df_cuttimes
+
+df_cuttimes.to_csv(pjoin('proc_data','cuttimes.csv'))
+
+#%%
+from mhdpy.mws_utils.coords import gen_coords_to_assign_1, assign_coords_multi
+dss_hvof = []
+dss_motor = []
+dss_filterwheel =[]
+for date in dates:
+    data_folder = mhdpy.io.gen_path('sharepoint', 'Data Share', 'MHD Lab', 'HVOF Booth', date)
+
+    dsst = mhdpy.io.TFxr(pjoin(data_folder, 'Processed_Data.tdms')).as_dsst()
+
+    dss_hvof.append(dsst['hvof'])
+    dss_motor.append(dsst['motor'])
+    dss_filterwheel.append(dsst['filterwheel'])
+
+
+# %%
+
+dsst = {
+    'hvof': xr.concat(dss_hvof, 'time'),
+    'motor': xr.concat(dss_motor, 'time'),
+    'filterwheel': xr.concat(dss_filterwheel, 'time')
+}
+# %%
+from mhdpy.io import dsst_to_tdms
+
+dsst_to_tdms(dsst, pjoin(DIR_PROC_DATA, 'dsst.tdms'), 'w')
---
+++ experiment/proc_add_coord.py
@@ -0,0 +1,102 @@
+#%%
+
+from mhdpy.analysis.standard_import import *
+
+
+dsst = mhdpy.io.TFxr(pjoin(DIR_PROC_DATA, 'dsst.tdms')).as_dsst()
+
+from mhdpy.io import load_df_cuttimes, extract_cuttime_list
+df_cuttimes = load_df_cuttimes(pjoin(DIR_PROC_DATA, 'cuttimes.csv'), reduce_columns=False)
+df_cuttimes = df_cuttimes.sort_values('Start Time').reset_index(drop=True)
+df_cuttimes = df_cuttimes.set_index(['Event', 'date'])
+
+cuttimes = extract_cuttime_list(df_cuttimes)
+timewindow = slice(cuttimes[0].start, cuttimes[-1].stop)
+
+
+ds_absem = xr.load_dataset(pjoin(DIR_PROC_DATA, 'ds_absem.cdf'))
+ds_absem = ds_absem.rename(time='acq_time')
+ds_lecroy = xr.load_dataset(pjoin(DIR_PROC_DATA, 'ds_lecroy.cdf'))
+#%%
+process_tcs = [
+    '53x',
+    '536_pos',
+    '536_power'
+    ]
+
+process_regex = ["({})_(\d)".format(s) for s in process_tcs]
+
+from mhdpy.mws_utils.coords import gen_coords_to_assign_1, assign_coords_multi
+import re
+
+def process_ds(ds, coords_to_assign, date, run_num, tc_base):
+
+    ds = assign_coords_multi(ds, coords_to_assign, min_mnum=10)
+    ds = ds.assign_coords(date=[date])
+
+    #TODO: believe these ohter coordinates are showing up because of low min_mnum
+    if tc_base == '53x':
+        if 'phi' in ds.dims:
+            ds = ds.sel(phi=0.8, method='nearest')
+
+
+    run_num = int(run_num)
+    ds = ds.assign_coords(run_num=[int(run_num)])#.expand_dims('run_num')
+    ds = ds.stack(run=['date','run_num'])
+    return ds
+
+coords_to_assign = gen_coords_to_assign_1(dsst)
+
+dss_absem = []
+
+from collections import defaultdict
+dss_absem = defaultdict(list)
+dss_lecroy = defaultdict(list)
+
+for (tc, date), row in df_cuttimes.iterrows():
+
+    sl = slice(
+        np.datetime64(row['Start Time']), 
+        np.datetime64(row['Stop Time'])
+        )
+
+
+    for regex in process_regex:
+        m = re.match(regex, tc)
+
+        if m:
+            tc_base, run_num = m.groups()
+
+            ds_absem_sel = ds_absem.sel(acq_time = sl)
+            ds_absem_sel = process_ds(ds_absem_sel, coords_to_assign, date, run_num, tc_base)
+            dss_absem[tc_base].append(ds_absem_sel)
+
+            ds_lecroy_sel = ds_lecroy.sel(acq_time = sl)
+            ds_lecroy_sel = process_ds(ds_lecroy_sel, coords_to_assign, date, run_num, tc_base)
+            dss_lecroy[tc_base].append(ds_lecroy_sel)
+
+
+
+#%%
+
+output_dir = pjoin(DIR_PROC_DATA, 'absem')
+if not os.path.exists(output_dir): os.mkdir(output_dir)
+
+for tc in dss_absem:
+    ds = xr.concat(dss_absem[tc], 'run')
+    ds = ds.unstack('run')
+
+    ds.to_netcdf(pjoin(output_dir, '{}.cdf'.format(tc)))
+
+# %%
+
+
+
+output_dir = pjoin(DIR_PROC_DATA, 'lecroy')
+if not os.path.exists(output_dir): os.mkdir(output_dir)
+
+for tc in dss_lecroy:
+    ds = xr.concat(dss_lecroy[tc], 'run')
+    ds = ds.unstack('run')
+
+    ds.to_netcdf(pjoin(output_dir, '{}.cdf'.format(tc)))
