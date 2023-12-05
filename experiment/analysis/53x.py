401af1f7e972b166793f3019b055636051fa8fec
Lee Aspitarte <aspitarte@gmail.com>
2023-12-05 14:03:18 -0800

    reorganize and continue 53x analysis

--- experiment/analysis/53x.py
+++ experiment/analysis/53x.py
@@ -5,6 +5,7 @@ from mhdpy.analysis.standard_import import *
 DIR_PROC_DATA = pjoin(REPO_DIR, 'experiment', 'data','proc_data')
 
 from mhdpy.mws_utils import calc_mag_phase_AS
+from mhdpy.plot import dropna
 
 # %%
 
@@ -17,12 +18,7 @@ ds_absem = ds_absem.assign_coords(run_plot = ('run', ds_absem.indexes['run'].val
 ds_absem['diff'] = ds_absem['led_on'] - ds_absem['led_off']
 ds_absem['alpha'] = 1 - ds_absem['diff']/ds_absem['calib']
 
-ds_lecroy = xr.load_dataset(pjoin(DIR_PROC_DATA, 'lecroy','{}.cdf'.format(tc)))
-ds_lecroy = ds_lecroy.stack(run=['date','run_num']).dropna('run',how='all')
-ds_lecroy = ds_lecroy.assign_coords(run_plot = ('run', ds_lecroy.indexes['run'].values))
 
-ds_lecroy = ds_lecroy.sortby('time') # Needed otherwise pre pulse time cannot be selected
-ds_lecroy = calc_mag_phase_AS(ds_lecroy)['AS']
 
 
 #%%
@@ -34,12 +30,7 @@ ds_lecroy = calc_mag_phase_AS(ds_lecroy)['AS']
 ds_absem['alpha'].mean('mnum').plot(col='mp', hue='run_plot',row='kwt', x='wavelength')
 plt.ylim(0,1.1)
 plt.xlim(760,780)
-# %%
-from mhdpy.plot import dropna
 
-g = ds_lecroy.mean('mnum').plot(hue='run_plot', row='kwt', x='time')
-
-dropna(g)
 
 #%%
 
@@ -84,9 +75,6 @@ dropna(g)
 plt.yscale('log')
 plt.xscale('log')
 
-#%%
-
-
 
 #%%
 
@@ -96,6 +84,18 @@ ds_nK = xr.merge([
 ])
 
 ds_nK
+
+
+#%%
+
+g = xr.plot.FacetGrid(ds_nK, col='mp', row='run')
+
+g.map(plt.errorbar, 'kwt', 'mean', 'std', capsize=5)
+
+plt.yscale('log')
+plt.xscale('log')
+
+dropna(g)
 
 #%%
 
@@ -114,12 +114,12 @@ plt.xscale('log')
 
 # Without any error propgation
 
-ds_sel_mean = ds_sel['mean'].mean('run')
-ds_sel_std = ds_sel['mean'].std('run')
+ds_nK_mean = ds_sel['mean'].mean('run')
+ds_nK_std = ds_sel['mean'].std('run')
 
 fig, axes = plt.subplots()
 
-xr_errorbar_axes(ds_sel_mean, ds_sel_std, axes)
+xr_errorbar_axes(ds_nK_mean, ds_nK_std, axes)
 
 plt.yscale('log')
 plt.xscale('log')
@@ -127,57 +127,136 @@ plt.xscale('log')
 
 #%%
 
-g = xr.plot.FacetGrid(ds_nK, col='mp', row='run')
 
-g.map(plt.errorbar, 'kwt', 'mean', 'std', capsize=5)
 
-plt.yscale('log')
-plt.xscale('log')
+
+#%%[markdown]
+
+# # Lecroy
+
+#%%
+
+ds_lecroy = xr.load_dataset(pjoin(DIR_PROC_DATA, 'lecroy','{}.cdf'.format(tc)))
+ds_lecroy = ds_lecroy.stack(run=['date','run_num']).dropna('run',how='all')
+ds_lecroy = ds_lecroy.assign_coords(run_plot = ('run', ds_lecroy.indexes['run'].values))
+
+ds_lecroy = ds_lecroy.sortby('time') # Needed otherwise pre pulse time cannot be selected
+ds_lecroy = calc_mag_phase_AS(ds_lecroy)
+
+# %%
+
+g = ds_lecroy.mean('mnum')['AS'].plot(hue='run_plot', row='kwt', x='time')
 
 dropna(g)
 
 #%%
 
+#TODO: make a class for dataarray containing uncertainty
 
+da_l_unc = ds_lecroy['mag'].mean('mnum')
+ds_l_unc = da_l_unc.to_dataset(name='mean')
+ds_l_unc['std'] = ds_lecroy['mag'].std('mnum')
 
-# %%
+ds_l_unc['count'] = ds_lecroy['mag'].mean('time').count('mnum')
 
-mws_max = ds_lecroy.mean('mnum').max('time')
-nK = ds_p['nK_m3'].sel(mp='barrel')
-
-#%%
-
-ds = xr.merge([mws_max,nK]).sortby('kwt')
-
-ds.unstack('run').plot.scatter(x='nK_m3', y='AS', hue='date')
+ds_l_unc
 
 #%%
 
-for run in ds.coords['run_plot']:
+# make facetgrid plot with confidence intervals
 
-    ds_sel = ds.sel(run=run).dropna('kwt')
-    if len(ds_sel.coords['kwt']):
-        ds_sel.set_coords('nK_m3')['AS'].sortby('nK_m3').plot(x='nK_m3', label=run.item(), marker='o')
-        # plt.plot(ds_sel['nK_m3'],ds_sel['AS'], label=run.item() ,marker='o')
+# Define your custom plotting function
+def custom_plot(time, mean, std, count, **kwargs):
+    lower_bound = mean - std
+    upper_bound = mean + std
+    plt.plot(time, mean, **kwargs)
+    plt.fill_between(time, lower_bound, upper_bound, alpha=0.5, color='gray')  # alpha sets the transparency
+    # count should be a single value, add it as text to the plot in the upper left
+    if not np.isnan(count):
+        plt.text(0.05, 0.9, '{}'.format(int(count)), transform=plt.gca().transAxes, color='red', fontsize=14, fontweight='bold')
 
-plt.legend()
 
-plt.xscale('log')
+
+g = xr.plot.FacetGrid(ds_l_unc, col='run', row='kwt')
+
+# Apply the custom function to each facet
+
+g.map(custom_plot, 'time', 'mean', 'std', 'count')
+
+plt.xlim(-10,10)
 
 #%%
 
-ds.sel(run=('2023-05-18', 1)).dropna('kwt')
+da_l_unc = ds_lecroy['AS'].mean('mnum')
+ds_l_unc = da_l_unc.to_dataset(name='mean')
+ds_l_unc['std'] = ds_lecroy['AS'].std('mnum')
+
+ds_l_unc['count'] = ds_lecroy['AS'].mean('time').count('mnum')
+ds_l_unc['sdom'] = ds_l_unc['std']/np.sqrt(ds_l_unc['count'])
+
+ds_l_unc
 
 #%%
 
+g = xr.plot.FacetGrid(ds_l_unc, col='run', row='kwt')
 
+# Apply the custom function to each facet
+
+g.map(custom_plot, 'time', 'mean', 'std', 'count')
+
+plt.xlim(-10,10)
+
+#%%
+
+g = xr.plot.FacetGrid(ds_l_unc, col='run', row='kwt')
+
+# Apply the custom function to each facet
+
+g.map(custom_plot, 'time', 'mean', 'sdom', 'count')
+
+
+#%%
+
+# calculate mean over run based on average weighted by sdom
+
+ds_l_unc['mean_weighted'] = ds_l_unc['mean']/ds_l_unc['sdom']**2
+
+dem = (1/ds_l_unc['sdom']**2).sum('run')
+
+ds_l_unc['mean_weighted'] = ds_l_unc['mean_weighted'].sum('run')/dem
+
+ds_l_unc['std_weighted'] = 1/np.sqrt(dem)
+
+ds_l_unc['count'] = ds_l_unc['count'].sum('run')
+
+ds_l_unc
+
+#%%
+
+ds2 = ds_l_unc[['mean_weighted', 'std_weighted', 'count']]
+
+ds2
+
+
+#%%
+
+g = xr.plot.FacetGrid(ds_l_unc, row='kwt')
+
+# Apply the custom function to each facet
+
+g.map(custom_plot, 'time', 'mean_weighted', 'std_weighted', 'count')
+
+# plt.ylim(0,1)
+
+
+#%%
 
 
 from mhdpy.mws_utils.fitting import fit_fn 
 from mhdpy.analysis.xr import fit_da_lmfit
 from lmfit import Model
 
-da_fit = ds_lecroy.mean('mnum').copy()
+da_fit = ds2['mean_weighted'].copy()
 
 
 pulse_max = da_fit.sel(time=slice(-1,1)).max('time')
@@ -220,7 +299,7 @@ da = ds.to_array('var')
 
 # for run in ds.coords['run_plot']:
 #     da_sel = da.sel(run=run)
-g= da.plot(col='run', x='time',hue='var', row='kwt')
+g= da.plot( x='time',hue='var', row='kwt')
 
 # plt.xscale('log')
 plt.yscale('log')
@@ -229,6 +308,60 @@ dropna(g)
 
 #%%
 
-ds_p['ne0'].plot(hue='run_plot', x='kwt', marker='o')
+ds_p['ne0'].plot( x='kwt', marker='o')
+
+#%%
+
+ds_p['ne0'].drop(0,'kwt').plot(marker='o')
+
 plt.yscale('log')
 plt.xscale('log')
+
+#%%
+
+ds_ne0 = ds_p['ne0'].to_dataset(name='mean')
+ds_ne0['std'] = ds_p_stderr['ne0']
+
+plt.errorbar(ds_ne0['kwt'], ds_ne0['mean'], ds_ne0['std'], marker='o', capsize=5)
+
+#%%[markdown]
+
+# # Compare Lecroy and MWS
+
+
+# %%
+
+mws_max = ds2.max('time')
+
+max_time = ds2['mean_weighted'].argmax('time')
+max_time
+#TODO: pretty sure isel is right here
+mws_max['std'] = ds2['std_weighted'].isel(time=max_time)
+
+
+#%%
+mws_max = mws_max.rename({'mean_weighted':'AS'})
+nK = ds_nK_mean.rename('nK_m3')
+
+#%%
+
+ds = xr.merge([mws_max,nK]).sortby('kwt')
+ds
+#%%
+
+ds.plot.scatter(x='nK_m3', y='AS')
+
+
+#%%
+
+plt.errorbar(ds['nK_m3'], ds['AS'], xerr=ds_nK_std, yerr=mws_max['std'], marker='o', capsize=5)
+
+plt.xscale('log')
+plt.yscale('log')
+
+#%%
+
+plt.errorbar(ds['nK_m3'], ds_ne0['mean'], xerr=ds_nK_std, yerr=ds_ne0['std'], marker='o', capsize=5)
+
+plt.xscale('log')
+
