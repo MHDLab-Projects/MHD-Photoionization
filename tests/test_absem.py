bb156e7d284609d293b3da1630d6466f60199952
Lee Aspitarte <aspitarte@gmail.com>
2023-12-14 18:38:39 -0800

    spectral model refactor and add absem fit test

---
+++ tests/test_absem.py
@@ -0,0 +1,35 @@
+#%%
+from mhdpy.analysis.standard_import import *
+
+from mhdpy.analysis.spectral import interp_alpha
+from mhdpy.analysis.spectral import model_blurredalpha_2peak
+from mhdpy.analysis.xr import fit_da_lmfit
+
+import pytest    
+
+
+def test_absem_fit():
+    """Tests a fit of a single absorption emssision spectrum and compares the fitted nK value to a previously obtained value"""
+
+    tc = '53x'
+
+    ds_absem = xr.load_dataset(pjoin(REPO_DIR, 'tests', 'test_data', 'absem', '{}.cdf'.format(tc)))
+    # Scipp cannot handle multindex, casts to a custom 'PyObject' dtype that does not go back to xarray
+
+    ds_absem['diff'] = ds_absem['led_on'] - ds_absem['led_off']
+    ds_absem['alpha'] = 1 - ds_absem['diff']/ds_absem['calib']
+
+    ds_sel = ds_absem.sel(mp='barrel').sel(date='2023-05-24').sel(run_num= 1).sel(kwt=1,method='nearest')
+    da_alpha = ds_sel['alpha'].dropna('mnum','all').mean('mnum')
+    da_alpha = da_alpha.sel(wavelength=slice(730,790))
+    da_alpha = interp_alpha(da_alpha)
+
+    final_model, pars = model_blurredalpha_2peak()
+
+    wls = da_alpha.coords['wavelength'].values
+    fits, ds_p, ds_p_stderr = fit_da_lmfit(da_alpha, final_model, pars, 'wavelength', wls)
+    ds_p['nK_m3'].attrs = dict(long_name='$n_{K,expt}$', units = '$\\#/m^3$')
+    # ds_p.coords['phi'].attrs = dict(long_name='Total Mass Flow', units = 'gram/second')
+    fits.name = 'alpha_fit'
+
+    assert ds_p['nK_m3'].item() == pytest.approx(1.32787e+22, rel=1e-4)
