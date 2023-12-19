b8dcc90a8702289a3ffc4f630d316444074b3ecb
Lee Aspitarte <aspitarte@gmail.com>
2023-12-19 04:35:03 -0800

    minor fixes to get notebooks working

--- experiment/analysis/tcs/536_pos.py
+++ experiment/analysis/tcs/536_pos.py
@@ -58,40 +58,14 @@ da_sel.plot(col='motor', hue='run_plot', x='time', figsize=(10,3))
 
 #%%
 
+from mhdpy.analysis.spectral import perform_fit_alpha
+
+spectral_reduction_params_fp = os.path.join(REPO_DIR,'experiment','metadata', 'spectral_reduction_params.csv')
+spect_red_dict = pd.read_csv(spectral_reduction_params_fp, index_col=0).squeeze().to_dict()
+
 ds_fit = ds_absem.mean('mnum')
 
-from mhdpy.analysis.spectral import gen_model_alpha_blurred, interp_alpha
-from mhdpy import analysis
-final_model, pars = gen_model_alpha_blurred()
-
-beta = -np.log(1-ds_fit['alpha'])/pars['L'].value
-beta_off = beta.sel(wavelength=slice(750,755)).mean('wavelength')
-alpha_tc = 1 - np.exp(-(beta - beta_off)*pars['L'].value)
-
-#%%
-
-#Downselect to remove data where there are no peaks... TODO: revisit
-# alpha_tc = alpha_tc.where(alpha_tc.sel(wavelength=slice(760,775)).max('wavelength') > 0.5).dropna('time', how='all')
-# alpha_tc = alpha_tc.sel(time=alpha_tc.coords['time'].values[::100])
-
-
-#%%
-
-spectral_reduction_params_fp = os.path.join(REPO_DIR, 'experiment', 'metadata', 'spectral_reduction_params.csv')
-spect_red_dict = pd.read_csv(spectral_reduction_params_fp, index_col=0).squeeze().to_dict()
-print('Reducing alpha with following data reduction parameters: ')
-print(spect_red_dict)
-alpha_tc_red = analysis.spectral.alpha_cut(alpha_tc,**spect_red_dict).dropna('wavelength', how='all')
-alpha_tc_red.name = 'alpha_red'
-
-wls = alpha_tc.coords['wavelength'].values
-fits, ds_p, ds_p_stderr = analysis.xr.fit_da_lmfit(alpha_tc_red, final_model, pars, 'wavelength', wls)
-ds_p['nK_m3'].attrs = dict(long_name='$n_{K,expt}$', units = '$\\#/m^3$')
-# ds_p.coords['phi'].attrs = dict(long_name='Total Mass Flow', units = 'gram/second')
-fits.name = 'alpha_fit'
-
-ds_alpha = xr.merge([alpha_tc, alpha_tc_red, fits]).sel(wavelength=slice(760,775))
-
+ds_p, ds_p_stderr, ds_alpha = perform_fit_alpha(ds_fit, spect_red_dict)
 
 #%%
 
