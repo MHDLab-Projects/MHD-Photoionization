724c3bf7ecb8cc8cbdc72c119043b5d34a1db138
Lee Aspitarte <aspitarte@gmail.com>
2023-12-18 20:50:26 -0800

    split out absem fitting test data into fixture

--- tests/test_absem.py
+++ tests/test_absem.py
@@ -7,10 +7,9 @@ from mhdpy.analysis.xr import fit_da_lmfit
 
 import pytest    
 
-
-def test_absem_fit():
-    """Tests a fit of a single absorption emssision spectrum and compares the fitted nK value to a previously obtained value"""
-
+@pytest.fixture
+def da_alpha():
+    
     tc = '53x'
 
     ds_absem = xr.load_dataset(pjoin(REPO_DIR, 'tests', 'test_data', 'absem', '{}.cdf'.format(tc)))
@@ -21,6 +20,12 @@ def test_absem_fit():
 
     ds_sel = ds_absem.sel(mp='barrel').sel(date='2023-05-24').sel(run_num= 1).sel(kwt=1,method='nearest')
     da_alpha = ds_sel['alpha'].dropna('mnum','all').mean('mnum')
+
+    return da_alpha
+
+def test_absem_fit_interpolated(da_alpha):
+    """Tests a fit of a single absorption emssision spectrum and compares the fitted nK value to a previously obtained value"""
+
     da_alpha = da_alpha.sel(wavelength=slice(730,790))
     da_alpha = interp_alpha(da_alpha)
 
@@ -28,8 +33,18 @@ def test_absem_fit():
 
     wls = da_alpha.coords['wavelength'].values
     fits, ds_p, ds_p_stderr = fit_da_lmfit(da_alpha, final_model, pars, 'wavelength', wls)
-    ds_p['nK_m3'].attrs = dict(long_name='$n_{K,expt}$', units = '$\\#/m^3$')
-    # ds_p.coords['phi'].attrs = dict(long_name='Total Mass Flow', units = 'gram/second')
-    fits.name = 'alpha_fit'
 
     assert ds_p['nK_m3'].item() == pytest.approx(1.32787e+22, rel=1e-4)
+
+
+def test_absem_fit_uninterpolated(da_alpha):
+    """Tests a fit of a single absorption emssision spectrum and compares the fitted nK value to a previously obtained value"""
+
+    da_alpha = da_alpha.sel(wavelength=slice(730,790))
+
+    final_model, pars = gen_model_alpha_blurred()
+
+    wls = da_alpha.coords['wavelength'].values
+    fits, ds_p, ds_p_stderr = fit_da_lmfit(da_alpha, final_model, pars, 'wavelength', wls)
+
+    assert ds_p['nK_m3'].item() == pytest.approx(1.2287e+22, rel=1e-4)
