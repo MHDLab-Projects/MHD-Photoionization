7024923f2d660d6f02b960a805ab98f86c869ff8
Lee Aspitarte <aspitarte@gmail.com>
2024-01-08 11:16:07 -0800

    change 53x notebook
    absem fit pipeline 2
    add kr fitting
    change handling of parameter stats

--- experiment/analysis/tcs/53x.py
+++ experiment/analysis/tcs/53x.py
@@ -1,5 +1,8 @@
-#%%
+#%%[markdown]
 
+# # 53x (seedramp) analysis
+
+#%%
 
 from mhdpy.analysis.standard_import import *
 DIR_PROC_DATA = pjoin(REPO_DIR, 'experiment', 'data','proc_data')
@@ -8,6 +11,10 @@ from mhdpy.analysis import mws
 from mhdpy.plot import dropna
 from mhdpy.xr_utils import WeightedMeanAccessor
 from mhdpy.analysis import absem
+
+#%%[markdown]
+
+# # Absem
 
 # %%
 
@@ -19,10 +26,6 @@ ds_absem = ds_absem.xr_utils.stack_run()
 ds_absem = ds_absem.absem.calc_alpha()
 ds_absem = ds_absem.sel(wavelength=slice(750,790))
 
-#%%
-
-# ds_absem['calib'].isel(run=[0,1]).mean('mnum').sel(mp='barrel').plot(hue='run_plot', x='wavelength', row='kwt')
-
 # %%
 
 ds_absem['alpha'].mean('mnum').plot(col='mp', hue='run_plot',row='kwt', x='wavelength')
@@ -32,28 +35,15 @@ plt.xlim(760,780)
 
 #%%
 
-from mhdpy.analysis.absem.fitting import pipe_fit_alpha_1
+from mhdpy.analysis.absem.fitting import pipe_fit_alpha_2
 
-spectral_reduction_params_fp = os.path.join(REPO_DIR,'experiment','metadata', 'spectral_reduction_params.csv')
-spect_red_dict = pd.read_csv(spectral_reduction_params_fp, index_col=0).squeeze().to_dict()
+ds_fit = ds_absem
 
-ds_fit = ds_absem.mean('mnum')
+ds_absem_fit, ds_p, ds_p_stderr = pipe_fit_alpha_2(ds_fit)
 
-ds_alpha_fit, ds_p, ds_p_stderr = pipe_fit_alpha_1(ds_fit, spect_red_dict)
-
-#%%
-
-ds = ds_alpha_fit[['alpha', 'alpha_fit']]
-ds = ds.rename({'alpha': 'data', 'alpha_fit':'fit'})
-ds = ds.to_array('var')
-
-ds
-
-ds_p.coords['kwt'].attrs = dict(long_name='K Mass Frac.', units='%')
-ds_p.coords['run_plot'].attrs = dict(long_name="Run")
 
 # %%
-da_plot = ds_alpha_fit[['alpha','alpha_fit']].to_array('var')
+da_plot = ds_absem_fit[['alpha_red','alpha_fit']].to_array('var')
 
 g = da_plot.sel(mp='barrel').plot(col='kwt',hue='var', row='run', ylim = (1e-3,2), yscale='log', figsize=(10,10))
 
@@ -84,8 +74,8 @@ ds_nK = xr.merge([
 ds_nK['std'] = ds_nK['stderr']*np.sqrt(ds_nK['count'])
 
 # ds_nK2 = ds_nK.wma.calc_weighted_mean('run')
-ds_nK2 = ds_nK.wma.initialize_stat_dataset('mean', 'run')
-ds_nK2
+# ds_nK2 = ds_nK.wma.initialize_stat_dataset('mean', 'run')
+# ds_nK2
 
 #%%[markdown]
 
@@ -97,34 +87,33 @@ ds_lecroy = xr.load_dataset(pjoin(DIR_PROC_DATA, 'lecroy','{}.cdf'.format(tc)))
 ds_lecroy = ds_lecroy.xr_utils.stack_run()
 
 ds_lecroy = ds_lecroy.sortby('time') # Needed otherwise pre pulse time cannot be selected
-ds_lecroy = ds_lecroy.mws.calc_mag_phase_AS()
+ds_lecroy = ds_lecroy.mws.calc_mag_phase_AS() # calculate before or after mnum mean?
+
+da_fit = ds_lecroy['AS'].mean('mnum')
 
 # %%
 
-g = ds_lecroy.mean('mnum')['AS'].plot(hue='run_plot', row='kwt', x='time')
+g = da_fit.plot(hue='run_plot', row='kwt', x='time')
 
 dropna(g)
 
+#%%[markdown]
 
-
-#%%
-
-ds_l_unc = ds_lecroy.wma.initialize_stat_dataset('AS', 'mnum')
-ds_l_unc_2 = ds_l_unc.wma.calc_weighted_mean('run')
+# ## Fixed kr, vary ne0
 
 #%%
 
 from mhdpy.analysis.mws.fitting import pipe_fit_mws_1 
 
-da_fit = ds_l_unc_2['mean'].copy().rename('AS')
-
 ds_mws_fit, ds_p, ds_p_stderr = pipe_fit_mws_1(da_fit)
+
+#%%
 
 da = ds_mws_fit.to_array('var')
 
 # for run in ds.coords['run_plot']:
 #     da_sel = da.sel(run=run)
-g= da.plot( x='time',hue='var', row='kwt')
+g= da.plot( x='time',hue='var', row='kwt', col='run')
 
 # plt.xscale('log')
 plt.yscale('log')
@@ -136,7 +125,34 @@ dropna(g)
 ds_ne0 = ds_p['ne0'].to_dataset(name='mean')
 ds_ne0['std'] = ds_p_stderr['ne0']
 
-plt.errorbar(ds_ne0['kwt'], ds_ne0['mean'], ds_ne0['std'], marker='o', capsize=5)
+plot.common.xr_errorbar(ds_ne0['mean'], ds_ne0['std'], huedim='run')
+
+#%%[markdown]
+
+# ## Fixed ne0, vary kr
+
+#%%
+
+from mhdpy.analysis.mws.fitting import gen_model_dnedt 
+
+mod, params = gen_model_dnedt(take_log=True)
+
+params['ne0'].value = 1e13 #TODO: value from cfd, needs to vary with K
+params['ne0'].vary = False
+params['kr'].vary = True
+
+ds_mws_fit, ds_p, ds_p_stderr = da_fit.mws.perform_fit(mod, params)
+
+
+#%%
+ds_kr = ds_p['kr'].to_dataset(name='mean')
+ds_kr['std'] = ds_p_stderr['kr']
+
+plot.common.xr_errorbar(ds_kr['mean'], ds_kr['std'], huedim='run')
+
+plt.yscale('log')
+plt.ylim(1e-15,2e-14)
+
 
 #%%[markdown]
 
@@ -145,26 +161,60 @@ plt.errorbar(ds_ne0['kwt'], ds_ne0['mean'], ds_ne0['std'], marker='o', capsize=5
 
 # %%
 
-mws_max = ds_l_unc_2.max('time')
-
-max_time = ds_l_unc_2['mean'].argmax('time')
-
-#TODO: pretty sure isel is right here
-mws_max['std'] = ds_l_unc_2['std'].isel(time=max_time)
-
-ds_nK2_barrel = ds_nK2.sel(mp='barrel')
+ds_lecroy['AS'].dropna('run', how='all').dropna('kwt', how='all')
 
 #%%
 
+da_stats = ds_lecroy['AS'].sel(time=slice(-1,1))
 
-ds = xr.merge([
-    mws_max.rename({'mean':'AS'})['AS'],
-    ds_nK2_barrel['mean'].rename('nK_m3')
-    ]).sortby('kwt')
+mws_max = da_stats.mean('mnum').max('time')
+
+mws_std = da_stats.std('mnum').max('time')
 
 #%%
 
-plt.errorbar(ds_nK2_barrel['mean'], mws_max['mean'], xerr=ds_nK2_barrel['std'], yerr=mws_max['std'], marker='o', capsize=5)
+ds_params = xr.merge([
+    ds_nK['mean'].sel(mp='barrel').drop('mp').rename('nK_m3'),
+    ds_ne0['mean'].rename('ne0'),
+    ds_kr['mean'].rename('kr'),
+    mws_max.rename('AS_max'),
+    mws_std.rename('AS_std')
+    ])
+
+ds_params
+
+
+#%%
+
+# Perform averages over run
+# TODO: combine with wma accessor
+
+count = ds_params['nK_m3'].count('run')
+
+ds_p_stats = xr.Dataset(coords=count.coords)
+
+for var in ds_params.data_vars:
+    mean = ds_params[var].mean('run')
+    std = ds_params[var].std('run')
+    stderr = std/np.sqrt(count)
+
+    ds_p_stats["{}_mean".format(var)] = mean
+    ds_p_stats["{}_std".format(var)] = std
+    ds_p_stats["{}_stderr".format(var)] = stderr
+#%%
+
+ds_p_stats
+
+
+#%%
+
+plt.errorbar(
+    ds_p_stats['nK_m3_mean'], 
+    ds_p_stats['AS_max_mean'], 
+    xerr=ds_p_stats['nK_m3_stderr'], 
+    yerr=ds_p_stats['AS_max_stderr'], 
+    marker='o', capsize=5
+    )
 
 plt.xscale('log')
 plt.yscale('log')
@@ -174,10 +224,39 @@ plt.xlabel("nK_m3")
 
 #%%
 
-plt.errorbar(ds_nK2_barrel['mean'], ds_ne0['mean'], xerr=ds_nK2_barrel['std'], yerr=ds_ne0['std'], marker='o', capsize=5)
+plt.errorbar(
+    ds_p_stats['nK_m3_mean'], 
+    ds_p_stats['ne0_mean'], 
+    xerr=ds_p_stats['nK_m3_stderr'], 
+    yerr=ds_p_stats['ne0_stderr'], 
+    marker='o', capsize=5
+    )
 
 plt.xscale('log')
+plt.yscale('log')
+
+plt.ylim(3e12,2e13)
+plt.xlim(4e20,2e22)
+
+plt.ylabel("Ne0")
+plt.xlabel("nK_m3")
+
+#%%
 
 
-plt.ylabel("ne_0 fit")
+plt.errorbar(
+    ds_p_stats['nK_m3_mean'], 
+    ds_p_stats['kr_mean'], 
+    xerr=ds_p_stats['nK_m3_stderr'], 
+    yerr=ds_p_stats['kr_stderr'], 
+    marker='o', capsize=5
+    )
+
+plt.xscale('log')
+plt.yscale('log')
+
+# plt.ylim(3e12,2e13)
+plt.xlim(4e20,2e22)
+
+plt.ylabel("Ne0")
 plt.xlabel("nK_m3")
