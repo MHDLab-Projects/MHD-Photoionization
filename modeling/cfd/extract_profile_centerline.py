6daa5bd9f60045c92b598841e43003f84ca8f953
Lee Aspitarte <aspitarte@gmail.com>
2024-01-09 10:06:23 -0800

    convert extract_profile_centerline to read multiple kwt

--- modeling/cfd/extract_profile_centerline.py
+++ modeling/cfd/extract_profile_centerline.py
@@ -11,49 +11,79 @@ from mhdpy.analysis.standard_import import *
 sp_dir = gen_path('sharepoint')
 
 results_dir = pjoin(sp_dir, 'Team Member Files', 'DaveH', 'Results', 'axiJP8200_17Jul23')
+
 # %%
-case = "mdot0130_phi080_K010"
-fname = os.path.join(results_dir, "medium",case,"frontCyl.vtk")
+def extract_line(mesh, fields):
 
-mesh = pv.read(fname)
+    unit_cm = 1e-2
+    x_exit = 20.8*unit_cm
 
-mesh
-# %%
-unit_cm = 1e-2
-x_exit = 20.8*unit_cm
+    y_pos_cm = 0
 
-y_pos_cm = 0
+    a = [x_exit - 1e-2 , y_pos_cm*unit_cm , 0]
+    b = [x_exit + 40e-2, y_pos_cm*unit_cm, 0]
+    line1 = mesh.sample_over_line(a, b)    
 
-a = [x_exit - 1e-2 , y_pos_cm*unit_cm , 0]
-b = [x_exit + 40e-2, y_pos_cm*unit_cm, 0]
-line1 = mesh.sample_over_line(a, b)    
+    data = np.array([
+        line1[species] for species in fields
+    ])
+
+    data = data.T
+
+    df_out = pd.DataFrame(data, index = line1.points[:,0], columns=all_fields)
+    df_out.index.name = 'x'
+
+    return df_out
+
+soi = ['K', 'Kp', 'em', 'OH', 'OHm', 'KOH']
+additional = ['T', 'p']
+all_fields = [*soi, *additional]
 
 #%%
 
-soi = ['K', 'Kp', 'em', 'OH', 'OHm', 'KOH']
 
-for species in soi:
+cases = [
+    "mdot0130_phi080_K000",
+    "mdot0130_phi080_K010",
+    "mdot0130_phi080_K100"
+]
 
-    plt.plot(line1.points[:,0], line1[species], label=species)
+kwts = [0,0.1,1] #TODO: regex
+
+
+dfs = []
+for i, case in enumerate(cases):
+    fname = os.path.join(results_dir, "medium",case,"frontCyl.vtk")
+
+    mesh = pv.read(fname)
+
+    df_out = extract_line(mesh, all_fields)
+
+    df_out['kwt'] = kwts[i]
+
+    dfs.append(df_out)
+
+
+df_lines = pd.concat(dfs)
+df_lines = df_lines.set_index('kwt', append=True)
+#%%
+
+ds = xr.Dataset.from_dataframe(df_lines)
+
+ds
+
+#%%
+
+ds[soi].to_array('species').plot(row='kwt', hue='species')
 
 plt.yscale('log')
 plt.ylabel("Species concentration")
 plt.xlabel("X Position")
-plt.legend()
+
 
 #%%
 
-additional = ['T', 'p']
-all_fields = [*soi, *additional]
 
-data = np.array([
-    line1[species] for species in all_fields
-])
 
-data = data.T
-
-df_out = pd.DataFrame(data, index = line1.points[:,0], columns=all_fields)
-df_out.index.name = 'x'
-
-df_out.to_csv('output/line_profiles.csv')
+ds.to_netcdf('output/line_profiles.cdf')
 # %%
