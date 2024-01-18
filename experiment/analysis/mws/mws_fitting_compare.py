9462237f52dfd7e5efa13eede69758ba63d5727e
Lee Aspitarte <aspitarte@gmail.com>
2024-01-17 16:56:30 -0800

    clean up mws_fit_compare and add exp model

--- experiment/analysis/mws/mws_fitting_compare.py
+++ experiment/analysis/mws/mws_fitting_compare.py
@@ -11,6 +11,8 @@ DIR_PROC_DATA = pjoin(REPO_DIR, 'experiment', 'data','proc_data')
 
 from mhdpy.analysis import mws
 
+dss_p = []
+
 #%%
 
 tc = '53x'
@@ -42,45 +44,9 @@ plt.xlim(-1,40)
 #%%
 
 from mhdpy.analysis.mws.fitting import gen_model_dnedt, pipe_fit_mws_1 
-
-pre_norm_cutoff = 5e-4
-da_fit = da_fit.where(da_fit.mws._pulse_max() > pre_norm_cutoff) # Targeting low power...
-
-mod, params = gen_model_dnedt()
-
-ds_mws_fit, ds_p, ds_p_stderr = da_fit.mws.perform_fit(mod, params)
-
-#%%
-
-da = ds_mws_fit.to_array('var')
-
-# for run in ds.coords['run_plot']:
-#     da_sel = da.sel(run=run)
-g= da.plot( x='time',hue='var', row='run')
-
-# plt.xscale('log')
-plt.yscale('log')
-
-
-#%%
-
-da_mean = ds_p['ne0']
-da_mean.name = 'mean'
-da_std = ds_p_stderr['ne0']
-da_std.name='std'
-
-df = xr.merge([da_mean, da_std]).to_dataframe()[['mean','std']]
-
-df.plot(marker='o', yerr='std', capsize=5)
-
-#%%
-ds_p
-
-#%%
-
-
-pre_norm_cutoff = 5e-4
-da_fit = da_fit.where(da_fit.mws._pulse_max() > pre_norm_cutoff) # Targeting low power...
+# pre_norm_cutoff=5e-4
+# da_fit = da_fit.where(da_fit.mws._pulse_max() > pre_norm_cutoff) # Targeting low power...
+da_fit = da_fit.mws.fit_prep(pre_norm_cutoff=5e-4, remove_negative=False)
 
 mod, params = gen_model_dnedt(take_log=False)
 
@@ -89,16 +55,19 @@ params['ne0'].vary = False
 params['kr'].vary = True
 
 ds_mws_fit, ds_p, ds_p_stderr = da_fit.mws.perform_fit(mod, params)
+
 #%%
 
-da_mean = ds_p['kr']
-da_mean.name = 'mean'
-da_std = ds_p_stderr['kr']
-da_std.name='std'
+da_fit
+#%%
 
-df = xr.merge([da_mean, da_std]).to_dataframe()[['mean','std']]
-df.plot(marker='o', yerr='std', capsize=5)
-plt.ylabel('kr')
+ds_p2 = xr.merge([
+    ds_p['kr'].rename('mean'),
+    ds_p_stderr['kr'].rename('std')
+    ])
+
+
+dss_p.append(ds_p2.assign_coords(method='avgmnum_1'))
 
 #%%
 ds_p
@@ -118,7 +87,6 @@ da_fit = da_sel.copy()
 
 ds_mws_fit, ds_p, ds_p_stderr = pipe_fit_mws_2(da_fit, take_log=False)
 
-# ds_mws_fit = ds_mws_fit.mean('mnum')
 
 
 #%%
@@ -145,13 +113,82 @@ for i, (group, ds) in enumerate(ds_mws_fit.groupby('run')):
 
 #%%
 
-da_mean = ds_p['kr']
-da_mean.name = 'mean'
-da_std = ds_p_stderr['kr']
-da_std.name='std'
+ds_p2 = xr.merge([
+    ds_p['kr'].rename('mean'),
+    ds_p_stderr['kr'].rename('std')
+    ])
 
-df = xr.merge([da_mean, da_std]).to_dataframe()[['mean','std']]
-df.plot(marker='o', yerr='std', capsize=5)
-plt.ylabel('kr')
-# %%
+dss_p.append(ds_p2.assign_coords(method='pipe_2'))
+
+#%%[markdown]
+
+# ## exponential fit
+
+#%%
+
+from mhdpy.analysis.mws.fitting import gen_model_exp
+
+mod, params = gen_model_exp()
+
+da_fit = da_sel.mean('mnum')
+
+ds_mws_fit, ds_p, ds_p_stderr = da_fit.mws.perform_fit(
+    mod, params,
+    fit_timewindow=slice(Quantity(5, 'us'),Quantity(20, 'us')) 
+    )
+
+ds_mws_fit = xr.merge([ds_mws_fit, da_fit.rename('AS_all')])
+
+#%%
+ds_mws_fit.to_array('var').plot(row='run',hue='var')
+
+plt.yscale('log')
+
+#%%
+
+ne0 = Quantity(2e12, 'cm**-3').to('um**-3').magnitude
+
+# tau = 1/(2*kr*ne0)
+# kr = 1/(2*tau*ne0)
+
+ds_p['kr'] = 1/(2*ne0*ds_p['decay'])
+
+ds_p['kr']
+
+#%%
+
+ds_p2 = xr.merge([
+    ds_p['kr'].rename('mean'),
+    ds_p['kr'].where(False).fillna(0).rename('std') # kr std not defined yet for exponential fit...
+    ])
+
+dss_p.append(ds_p2.assign_coords(method='exp'))
+
+#%%
+
+ds_p = xr.concat(dss_p, dim='method')
+
 ds_p
+
+#%%
+
+# Define your custom plotting function
+def custom_plot(x, mean, std, **kwargs):
+    x = [str(t) for t in x]
+    plt.errorbar(x, mean, yerr=std, capsize=4, marker='o')
+
+
+fg = xr.plot.FacetGrid(ds_p, row='method')
+
+# Apply the custom function to each facet
+
+fg.map(custom_plot, 'run', 'mean', 'std')
+
+
+#%%
+
+for method, ds in ds_p.groupby('method'):
+    ds = ds.squeeze() # having to do this 
+    plt.errorbar(ds['run_plot'], ds['mean'], yerr=ds['std'], capsize=4, marker='o', label=method)
+
+plt.legend()
