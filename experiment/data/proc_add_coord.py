9aec4700a84226f73ed058fccad23d81d7946463
Lee Aspitarte <aspitarte@gmail.com>
2024-02-03 12:08:45 -0800

    refactor proc_add coord
    change data flow to be able to assign general cuttimes

--- experiment/data/proc_add_coord.py
+++ experiment/data/proc_add_coord.py
@@ -33,7 +33,7 @@ df_cuttimes = load_df_cuttimes(pjoin(REPO_DIR, 'experiment', 'metadata', 'cuttim
 
 # add tc and run_num columns. tc is everything before the last underscore in 'Event' column
 df_cuttimes['tc'] = df_cuttimes['Event'].str.extract(r'(.*)_\d$')
-df_cuttimes['run_num'] = df_cuttimes['Event'].str.extract(r'.*_(\d)$')
+df_cuttimes['run_num'] = df_cuttimes['Event'].str.extract(r'.*_(\d)$').astype(int)
 
 
 process_tcs = [
@@ -48,45 +48,75 @@ process_tcs = [
 # Only keep columns with tc in process_tcs
 df_cuttimes = df_cuttimes[df_cuttimes['tc'].isin(process_tcs)]
 
-df_cuttimes = df_cuttimes.sort_values('Start Time').set_index(['tc', 'run_num' ,'date'])
 
 
 #%%
-def process_ds(ds, coords_to_assign, date, run_num, tc_base, min_mnum=10):
 
-    ds = assign_coords_multi(ds, coords_to_assign, min_mnum=min_mnum)
+def assign_coords_timewindows(ds, coords_to_assign, df_cuttimes, min_mnum=None, timeindex='acq_time', downselect_dict=None):
+    """
+    Splits ds into time windows and applies coords_to_assign. 
 
-    #TODO: believe these ohter coordinates are showing up because of low min_mnum
-    if tc_base == '53x':
-        if 'phi' in ds.dims:
-            ds = ds.sel(phi=0.8, method='nearest')
+    min_mnum: minimum number of measurements that must be present in each timewindow 
+    downselect_dict: dictionary of coordinates to downselect to a signle value before concatenating. indexes cannot vary between timewindows
+    """
 
-    ds = ds.assign_coords(date=[date])
-    ds = ds.assign_coords(run_num=[int(run_num)])#.expand_dims('run_num')
-    ds = ds.stack(run=['date','run_num']) #TODO: this can not be replaced by xr_utils.stack_run, debug
-    return ds
+    #TODO: generalize to not require date/run_num
+    required_cols = ['Start Time', 'Stop Time', 'date', 'run_num']
+    for col in required_cols:
+        assert col in df_cuttimes.columns, "df_cuttimes needs to have {} column for this function".format(col)
 
+    df_cuttimes = df_cuttimes.sort_values('Start Time')#.set_index(['tc', 'run_num' ,'date'])
+
+    dss = []
+    
+    for idx, row in df_cuttimes.iterrows():
+
+        sl = slice(
+            np.datetime64(row['Start Time']), 
+            np.datetime64(row['Stop Time'])
+            )
+
+        ds_tw = ds.sel({timeindex: sl})
+
+        ds_tw = assign_coords_multi(ds_tw, coords_to_assign, min_mnum=min_mnum)
+
+        if downselect_dict is not None:
+            for coord, value in downselect_dict.items():
+                if coord in ds_tw.dims:
+                    ds_tw = ds_tw.sel({coord: value}, method='nearest')
+
+        ds_tw = ds_tw.assign_coords(date=[row['date']])
+        ds_tw = ds_tw.assign_coords(run_num=[row['run_num']])
+
+        ds_tw = ds_tw.stack(run=['date','run_num']) #TODO: this can not be replaced by xr_utils.stack_run, debug
+
+        dss.append(ds_tw)
+
+    ds_out = xr.concat(dss, 'run')
+
+    return ds_out
+    
 
 dss_absem = defaultdict(list)
 dss_lecroy = defaultdict(list)
 
-for (tc_base, run_num, date), row in df_cuttimes.iterrows():
+for tc_base, df_cuttimes_tc in df_cuttimes.groupby('tc'):
 
-    sl = slice(
-        np.datetime64(row['Start Time']), 
-        np.datetime64(row['Stop Time'])
-        )
+    #TODO: believe these ohter coordinates are showing up because of low min_mnum
+    if tc_base == '53x':
+        # if 'phi' in ds.dims:
+        downselect_dict = {'phi': 0.8}
+    else:
+        downselect_dict = None
 
-    ds_absem_sel = ds_absem.sel(acq_time = sl)
+
     #TODO: can we add 'mnum_counts' as a variable in a general way and downselect later?
-    ds_absem_sel = process_ds(ds_absem_sel, coords_to_assign, date, run_num, tc_base, min_mnum=2) #Absem aready downselected...
+    ds_absem_sel = assign_coords_timewindows(ds_absem, coords_to_assign, df_cuttimes_tc, min_mnum=2, downselect_dict=downselect_dict)
+    ds_lecroy_sel = assign_coords_timewindows(ds_lecroy, coords_to_assign, df_cuttimes_tc, min_mnum=10 , downselect_dict=downselect_dict)
+
+
     dss_absem[tc_base].append(ds_absem_sel)
-
-    ds_lecroy_sel = ds_lecroy.sel(acq_time = sl)
-    ds_lecroy_sel = process_ds(ds_lecroy_sel, coords_to_assign, date, run_num, tc_base, min_mnum=10)
     dss_lecroy[tc_base].append(ds_lecroy_sel)
-
-
 #%%
 
 output_dir = pjoin(DIR_PROC_DATA, 'absem')
@@ -94,6 +124,9 @@ if not os.path.exists(output_dir): os.mkdir(output_dir)
 
 for tc in dss_absem:
     ds = xr.concat(dss_absem[tc], 'run')
+
+    # ds['led_on'] = ds['led_on']*2
+
     ds = ds.unstack('run')
 
     ds.to_netcdf(pjoin(output_dir, '{}.cdf'.format(tc)))
