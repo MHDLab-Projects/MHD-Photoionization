f6b3a61ff3a0de5996e6beb328a178e652de1302
Lee Aspitarte <aspitarte@gmail.com>
2024-02-14 09:44:26 -0800

    add coordinate setting to spe

--- experiment/notebook/2023-05-18/spe_analysis.py
+++ experiment/notebook/2023-05-18/spe_analysis.py
@@ -99,6 +99,59 @@ axes[1].set_xlabel('')
 plt.savefig(pjoin(DIR_FIG_OUT, 'spe_gatedelay_img.png'), bbox_inches='tight', dpi=300)
 
 #%%
+DIR_PROC_DATA = pjoin(REPO_DIR, 'experiment', 'data','proc_data')
+
+fp_dsst = pjoin(DIR_PROC_DATA, 'dsst.tdms')
+dsst = mhdpy.fileio.TFxr(fp_dsst).as_dsst()
+
+fp_dst_coords = pjoin(DIR_PROC_DATA, 'dst_coords.tdms')
+dst_coords = mhdpy.fileio.TFxr(fp_dst_coords).as_dsst()['coords']
+
+dst_coords
+
+#%%
+
+from mhdpy.coords import assign_signal, unstack_multindexed_acq_dim
+
+dst_coords['motor'].sel(time=slice(da.coords['estime'].min(), da.coords['estime'].max())).plot()
+
+# assign_signal(da, dst_coords['motor'])
+#%%
+
+tw_536_pos_1 = slice(Timestamp('2023-05-18 22:23:34.172281344'), Timestamp('2023-05-18 22:30:20.482833664'), None)
+tw_536_pos_2 = slice(Timestamp('2023-05-18 22:45:57.737082368'), Timestamp('2023-05-18 22:52:50.292869120'), None)
+# tw_536_pos_both = slice(Timestamp('2023-05-18 22:21:30.395011328'), Timestamp('2023-05-18 22:57:37.093530624'), None)
+
+da_536_pos =  xr.concat([
+    da.sel(estime=tw_536_pos_1),
+    da.sel(estime=tw_536_pos_2),
+],
+    dim='estime'
+)
+
+
+da_536_pos = assign_signal(da_536_pos, dst_coords['motor'], timeindex='estime')
+
+da_536_pos = unstack_multindexed_acq_dim(da_536_pos, 'estime')['counts']
+
+da_536_pos
+
+#%%
+
+da_536_pos.mean('mnum').sel(gatedelay=800).plot(col='motor', col_wrap=4, robust=True)
+
+
+#%%
+
+tw_536_fullpow = slice(Timestamp('2023-05-18 23:09:06.592949504'), Timestamp('2023-05-18 23:10:35.851570944'), None)
+
+da_536_fullpow = da.sel(estime=tw_536_fullpow).mean('estime')
+
+da_536_fullpow.sel(gatedelay=[800,850]).plot(col='gatedelay', robust=True)
+
+
+
+#%%
 
 da = das['63']
 
