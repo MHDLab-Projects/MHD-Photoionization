492de094a791d84927dd1b92c8c84375f1964ed6
Lee Aspitarte <aspitarte@gmail.com>
2024-02-14 09:44:40 -0800

    additional analysis O2 capture

--- experiment/analysis/mws/mws_53x_kr_compare.py
+++ experiment/analysis/mws/mws_53x_kr_compare.py
@@ -85,20 +85,44 @@ ds_cfd['Yeq_K+'].plot()
 
 #%%
 
+ds_cfd['T']
+
+#%%
+
 ds_cfd['rho'].pint.to('1/cm^3').plot()
 
 #%%
 
-ds_cfd['O2'].plot()
+ds_cfd[['N2','O2','CO2','H2O']].to_array('var').plot(hue='var')
+#%%
+
+# combining k4 from 
+# 1. Axford, S.D.T., and Hayhurst, A.N. (1997). Mass spectrometric sampling of negative ions from flames of hydrogen and oxygen: the kinetics of electron attachment and detachment in hot mixtures of H2O, O2, OH and HO2. Proceedings of the Royal Society of London. Series A: Mathematical, Physical and Engineering Sciences 452, 1007–1033. 10.1098/rspa.1996.0051.
+
+
+k4_species = {
+    'N2' : Quantity(1e-32, 'cm^6/s'),
+    'H2O': Quantity(8e-30, 'cm^6/s'),
+    'CO2': Quantity(1e-31, 'cm^6/s'), 
+    'O2' : Quantity(2e-30, 'cm^6/s'),
+}
+
+weighted_avg = 0
+
+for species, k4 in k4_species.items():
+    weighted_avg += k4*ds_cfd[species]
+
+weighted_avg
 
 #%%
 
 rxn_rates = {
     'K+':  Quantity(4e-24, 'K*cm^6/s')*(1/ds_cfd['T'])*ds_cfd['rho'],
     'OH': Quantity(3e-31, 'cm^6/s')*ds_cfd['rho'],
-    'O2': Quantity(1e-30, 'cm^6/s')*ds_cfd['rho'],
+    # 'O2': Quantity(5e-31, 'cm^6/s')*ds_cfd['rho'],
+    'O2': weighted_avg,
     # 'O2': Quantity(6e-34, 'cm^6/s')*ds_cfd['rho'],
-    'H2O': Quantity(1e-8, 'cm^3/s')*np.exp(-(Quantity(35000, 'K')/ds_cfd['T'])),
+    'H2O': Quantity(1.6e-6, 'cm^3/s')*np.exp(-(Quantity(36060, 'K')/ds_cfd['T'])),
 }
 
 ds_kr = xr.Dataset(rxn_rates).pint.to('cm^3/s')
@@ -115,8 +139,9 @@ plt.yscale('log')
 
 #%%
 
+# Factor of 2 only occurs for K+
 
-ds_species_decay = 1/(2*ds_p['decay'].pint.quantify('us')*ds_kr)
+ds_species_decay = 1/(ds_p['decay'].pint.quantify('us')*ds_kr)
 
 ds_species_decay = ds_species_decay.pint.to('1/cm^3')
 
@@ -151,18 +176,36 @@ ds_species_cfd
 
 #%%
 
-ds_tau = 1/(2*ds_species_cfd*ds_kr)
+ds_tau = 1/(ds_species_cfd*ds_kr)
 
 ds_tau = ds_tau.pint.to('us')
 
 ds_tau
 
 #%%
+for species in ds_tau.data_vars:
+    ds_tau[species].plot(marker='o', label="CFD: {}".format(species))
 
-ds_tau.to_array('species').plot(hue='species', marker='o')
+ds_p['decay'].mean('run').plot(marker='o', label='Experiment')
+
+plt.legend()
+plt.yscale('log')
+plt.ylabel('Decay constant (us)')
+# %%
+
+ds_tau['O2'].plot()
 
 ds_p['decay'].mean('run').plot(marker='o', label='experiment')
 
-# plt.legend()
+plt.yscale('log')
+# %%
+
+tau_exp = ds_p['decay'].mean('run').pint.quantify('us')
+k_eff = 1/(tau_exp*ds_cfd['rho']**2)
+
+k_eff = k_eff.pint.to('cm^6/s')
+
+k_eff.plot(marker='o')
+
 plt.yscale('log')
 # %%
