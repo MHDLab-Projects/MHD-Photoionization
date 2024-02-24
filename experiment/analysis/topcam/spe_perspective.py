294e7a3e17b44fd065dd30a7bbc34268209aa76b
Lee Aspitarte <aspitarte@gmail.com>
2024-02-23 21:23:12 -0800

    further work spe perspective

--- experiment/analysis/topcam/spe_perspective.py
+++ experiment/analysis/topcam/spe_perspective.py
@@ -16,16 +16,47 @@ fp_in = pjoin(REPO_DIR, 'experiment','data','manual', 'PI Max Calibration', 'Log
 img = imread(fp_in)
 imshow(img)
 
+#%%
+img.shape
+
 # %%
 # Convert the image from BGR to RGB for proper visualization with Matplotlib
 # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-# Define the source points
-src = np.array([251, 478,
-                716, 474,
-                251, 549,
-                715, 541,
+# # Define the source points
+# src = np.array([251, 478,
+#                 716, 475,
+#                 251, 550,
+#                 715, 540,
+# ]).reshape((4, 2))
+
+
+# Define the source points no y distorion (ruler may not have been held flat)
+# Measure the width of the ruler in pixels. then calculate the positions based on a flat ruler
+# Tweaked final value unti output image ruler had consistent inch spacing
+# This will scale the image but not correct for y distortion
+center_value = 512
+dy_barrel = 73
+dy_horns = 66
+
+y_barrel_1 = center_value - dy_barrel/2
+y_barrel_2 = center_value + dy_barrel/2
+
+y_horns_1 = center_value - dy_horns/2
+y_horns_2 = center_value + dy_horns/2
+
+src = np.array([251, y_barrel_2,
+                716, y_horns_2,
+                251, y_barrel_1,
+                715, y_horns_1,
 ]).reshape((4, 2))
+
+# # Extreme distorion for testing 
+# src = np.array([251, 476,
+#                 716, 496,
+#                 251, 545,
+#                 715, 565,
+# ]).reshape((4, 2))
 
 # Plot the image
 plt.imshow(img)
@@ -34,10 +65,10 @@ plt.imshow(img)
 plt.scatter(src[:, 0], src[:, 1], color='red', marker='o')
 
 # %%
-dst = np.array([251, 477,
-                715, 477,
-                251, 547,
-                715, 547,
+dst = np.array([251, 476,
+                715, 476,
+                251, 545,
+                715, 545,
 ]).reshape((4, 2))
 tform = transform.estimate_transform('projective', src, dst)
 
@@ -47,14 +78,20 @@ tf_img = transform.warp(img, tform.inverse)
 fig, ax = plt.subplots()
 ax.imshow(tf_img)
 
+
+#%%
+
+# save the transformation matrix
+
+fp_out = pjoin(DIR_DATA_OUT, 'perspective_transform_matrix.npy')
+np.save(fp_out, tform.params)
+
+
 #%%
 
 fp_out = pjoin(DIR_DATA_OUT, 'img_projective.tif')
 
 # Save the image
-
-
-
 
 tf_img_out = tf_img*255
 # Convert the image data to 8-bit unsigned integers
@@ -73,14 +110,103 @@ ds = xr.load_dataset(fp_test_spe)
 
 ds['counts'].mean('gatedelay').mean('estime').plot()
 
-#%%
-ds.coords['x']
+
 
 #%%
 
-xgrid = np.arange(0, 1024, 1)
-ygrid = np.arange(0, 1024, 1)
+ds_sel = ds.mean('estime')
 
-xgrid, ygrid = np.meshgrid(xgrid, ygrid)
 
-xgrid
+#%%
+
+#%%
+
+da_test = ds_sel.isel(gatedelay=0)['counts']
+
+xs = np.linspace(0, 1024, 1024)
+ys = np.linspace(0, 1024, 1024)
+
+da_test_int = da_test.interp(x=xs, y=ys).fillna(0)
+
+
+img_test = da_test_int.values
+
+
+plt.axhline(50, color='r')
+
+imshow(img_test[460:550, 100:600])
+# imshow(img_test)
+
+#%%
+
+img_test_tf = transform.warp(img_test, tform.inverse)
+
+
+# zoom in 
+plt.axhline(50, color='r')
+
+imshow(img_test_tf[460:550, 100:600])
+# imshow(img_test_tf)
+
+#%%
+# interpolate to 2d grid and use skimage.transform.warp to apply the transformation
+
+from utils import transform_projective
+
+x_grid = np.arange(0, 1024, 1)
+y_grid = np.arange(0, 1024, 1)
+ds_sel_int = ds_sel.interp(x=xs, y=ys)#.fillna(0)
+
+da_tf = ds_sel_int.groupby('gatedelay').apply(lambda x: transform_projective(x, tform))
+
+# downsel_range = {'x': slice(0, 1024), 'y': slice(412, 612)}
+# downsel_range = {'x': slice(0, 1024), 'y': slice(312, 712)}
+downsel_range = {'x': slice(0, 1024), 'y': slice(460, 550)}
+
+da_tf = da_tf.sel(downsel_range)
+#%%
+
+da_tf.plot(col='gatedelay')
+
+
+
+
+# %%
+
+ds_int
+
+
+
+# plt.xlim(400, 600)
+
+
+
+# %%
+#%%
+
+# Trying to apply the transformation to the xarray dataset
+
+
+# ds_stack = ds_sel.stack(temp=['x','y'])
+
+# ds_stack
+
+# mat = tform.inverse.params
+
+# def transform_projective(ds, mat):
+#     x = ds['x'].item()
+#     y = ds['y'].item()
+#     ds = ds.drop('temp')
+
+#     x_new, y_new, z = np.dot(mat, [x, y, 1])
+
+#     ds['x'] = x_new
+#     ds['y'] = y_new
+
+#     ds = ds.expand_dims('x').expand_dims('y').stack(temp=['x','y'])
+
+#     return ds
+
+
+# ds_stack.groupby('temp').apply(lambda x: transform_projective(x, mat))
+#
