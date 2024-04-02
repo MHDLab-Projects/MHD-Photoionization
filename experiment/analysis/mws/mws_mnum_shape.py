7af8e9f343a63f28e316f1a2b784975b971ff66e
Lee Aspitarte <aspitarte@gmail.com>
2024-04-02 12:02:08 -0700

    add position dependence to mnum shape

--- experiment/analysis/mws/mws_mnum_shape.py
+++ experiment/analysis/mws/mws_mnum_shape.py
@@ -31,7 +31,7 @@ n_mnums = 7
 
 # pick an evenly space selection of mnums
 
-mnums_sel = np.linspace(mnums.min(), mnums.max(), n_mnums).astype(int)
+mnums_sel = np.linspace(mnums.min()+1, mnums.max(), n_mnums).astype(int)
 
 mnums_sel
 
@@ -103,4 +103,45 @@ da_hp.mean('mnum').plot(hue='run_plot', x='time')
 # plt.xlim(-1,30)
 plt.ylim(1e-3,)
 plt.yscale('log')
+# %%
+ds_lecroy = ppu.fileio.load_lecroy('536_pos', AS_calc='absolute')
+
+ds_lecroy
+
+#%%
+ds_sel = ds_lecroy.sel(run=('2023-05-18', 1))
+da_sel = ds_sel['AS_abs']
+da_sel = da_sel.dropna('motor', how='all')
+
+motor_sel = [30, 50, 80, 105, 130, 150, 180, 210]
+
+da_sel = da_sel.sel(motor = motor_sel, method='nearest')
+da_sel = da_sel.dropna('mnum', how='all')
+
+# da_sel = da_sel.xr_utils.assign_mnum('mnum')    
+da_sel = da_sel.groupby('motor').apply(lambda x: x.xr_utils.assign_mnum('mnum'))
+
+da_sel
+
+#%%
+
+
+mnums = da_sel.coords['mnum'].values
+n_mnums = 5
+
+# pick an evenly space selection of mnums
+
+mnums_sel = np.linspace(mnums.min()+1, mnums.max(), n_mnums).astype(int)
+
+mnums_sel
+
+
+# da_sel['AS_abs']
+# %%
+
+da_sel.sel(mnum=mnums_sel).plot(hue='mnum', col='motor', col_wrap=2)
+
+
+plt.savefig(pjoin(DIR_FIG_OUT, 'mws_536_pos_mnum_sample.png'))
+# plt.
 # %%
