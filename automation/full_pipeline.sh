08537dceb0c97cd049348ddb29b7cf545a47a9bc
Lee Aspitarte <aspitarte@gmail.com>
2024-06-18 16:50:39 -0700

    add full pipeline automation

---
+++ automation/full_pipeline.sh
@@ -0,0 +1,16 @@
+# source munge.sh
+
+source ../.env
+
+source process_munged.sh
+source supp_scripts.sh
+source final_dataset.sh
+source render_all_expt.sh
+
+source gen_fig_panels.sh
+
+source render_figures.sh
+
+source gen_docs.sh
+
+source collect_final_files.sh
