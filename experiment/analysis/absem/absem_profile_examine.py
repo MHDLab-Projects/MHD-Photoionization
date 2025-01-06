c4e6fb46f4aa0346cd2a3a34c49158fcf398c635
Lee Aspitarte <aspitarte@gmail.com>
2025-01-06 14:10:44 -0800

    add collisional cross seciton broadening

--- experiment/analysis/absem/absem_profile_examine.py
+++ experiment/analysis/absem/absem_profile_examine.py
@@ -26,3 +26,37 @@ plt.legend()
 
 plt.xlim(766.4,766.6)
 # %%
+
+# Calculating collisional broadening from Alkemade II.231 and Table VII.9
+
+
+# sigma = Quantity(60.4, 'angstrom^2')
+sigma = Quantity(57.7, 'angstrom^2')
+T = Quantity(2500, 'K')
+
+m_N2 = Quantity(28, 'amu')
+m_K = Quantity(39, 'amu')
+
+redmass_N2 = m_N2*m_K/(m_N2+m_K)
+
+kb = Quantity(1, 'boltzmann_constant')
+
+p = Quantity(1, 'atm')
+
+nx = p/(kb*T)
+
+vth = np.sqrt(8*kb*T/(redmass_N2*np.pi))
+
+# Alkemade formula. Not sure where this pi is coming from
+# nu_c = vth*sigma*nx/np.pi
+
+nu_c = vth*sigma*nx
+
+nu_c.to('1/ns')
+#%%
+
+vth.to('cm/s')
+
+#%%
+
+nx.to('1/m^3')
