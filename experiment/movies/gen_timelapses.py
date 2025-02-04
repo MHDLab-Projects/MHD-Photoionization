50b534980060a596c4490f5048114ce95e4881d7
Lee Aspitarte <aspitarte@gmail.com>
2025-02-04 10:33:23 -0800

    add setup movie to timelapse output

--- experiment/movies/gen_timelapses.py
+++ experiment/movies/gen_timelapses.py
@@ -16,25 +16,36 @@ fp_df_dav_meta = os.path.join('output', 'flir', 'df_dav_meta.csv')
 fp_timewindows = os.path.join(os.getenv('repo_dir'), 'experiment', 'metadata', 'ct_experiment.csv')
 df_ct = load_df_cuttimes(fp_timewindows).set_index('Event')
 
-dates = ['2023-04-07', '2023-05-12', '2023-05-18', '2023-05-24']
+fp_timewindows_extra = os.path.join('ct_movie.csv')
+df_ct_add = load_df_cuttimes(fp_timewindows_extra).set_index('Event')
+
+df_ct = pd.concat([df_ct, df_ct_add])
+
+# idxs = ['2023-04-07', '2023-05-12', '2023-05-18', '2023-05-24']
+idxs = ['2023-05-18', '2023-05-18_setup']
 channels = ['5','6','7','8']
 
-for date in dates:
+for idx in idxs:
 
     #TODO: FLIR timestamps are in PT. See issue #13
-    ts_start = Timestamp(df_ct.loc[date]['Start Time'], tz='UTC').tz_convert('US/Pacific').tz_localize(None)
-    ts_stop = Timestamp(df_ct.loc[date]['Stop Time'], tz='UTC').tz_convert('US/Pacific').tz_localize(None)
+    ts_start = Timestamp(df_ct.loc[idx]['Start Time'], tz='UTC').tz_convert('US/Pacific').tz_localize(None)
+    ts_stop = Timestamp(df_ct.loc[idx]['Stop Time'], tz='UTC').tz_convert('US/Pacific').tz_localize(None)
 
     output_timewindow = slice(
         ts_start,
         ts_stop
         )
 
+    if 'setup' in idx:
+        date = idx.split('_')[0]
+    else:
+        date = idx
+
     date = Timestamp(date).date()
 
     folder_in = os.path.join(os.getenv('RAW_FLIR_FOLDER'), date.strftime("%Y"))
 
-    folder_out = os.path.join('output', 'flir', date.strftime("%Y-%m-%d"))
+    folder_out = os.path.join('output', 'flir', idx)
     if not os.path.exists(folder_out): os.makedirs(folder_out)
 
     dfs_meta = []
@@ -47,7 +58,7 @@ for date in dates:
         # maybe integrate with the for loop in the ffmpeg_davs_to_mp4 fn 
         dfs_meta.append(df_dav_meta)
 
-        fn_out = 'Timelapse_{}_ch{}.mp4'.format(date.strftime("%Y-%m-%d"), channel)
+        fn_out = 'Timelapse_{}_ch{}.mp4'.format(idx, channel)
         fp_out = os.path.join(folder_out, fn_out)
 
         if os.path.exists(fp_out):
