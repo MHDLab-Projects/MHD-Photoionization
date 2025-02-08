4c61dfb8371d26b871c4ffce8493bfd2677bf61f
Dublin Nichols <dubdubohio@gmail.com>
2025-02-07 16:37:10 -0800

    changed figures to vertical; added labels directly using matplotlib

--- final/figure_panels/position_dependence.py
+++ final/figure_panels/position_dependence.py
@@ -45,11 +45,54 @@ ds_cfd['nK_cm3'] = ds_cfd[ppu.CFD_K_SPECIES_NAME].pint.to('particle/cm^3')
 
 #%%
 
-fig, axes = plt.subplots(2, figsize=(5,5), sharex=True)
+nrows = 3
+fig = plt.figure()
 
+fig.set_figheight(nrows * 3)
+fig.set_figwidth(5)
+
+# fig.set_figheight(8)
+
+sfigs = fig.subfigures(2,1, height_ratios = [1,2], hspace = 0)
+
+sfigs[0].subplots(ncols = 3, sharey = True)
+
+
+ds_plot = ds_alpha_fit.sel(run=('2023-05-18', 1)).sel(phi=0.8, method='nearest')
+
+# da_plot.plot(col='phi', hue='var', row='motor')
+motor_sel = [80, 130, 180]
+
+ds_plot = ds_plot.sel(motor=motor_sel, method='nearest')
+
+# fig, axes = plt.subplots(1,len(motor_sel), figsize=(5,2), sharey=True)
+
+axes = sfigs[0].get_axes()
+
+for i, motor in enumerate(ds_plot.coords['motor']):
+    ax = axes[i]
+    ds_plot.sel(motor=motor).to_array('var').plot(hue='var', ax=ax)
+    ax.set_title('{:.0f} mm'.format(motor))
+
+    ax.set_xlabel("Wavelength [nm]")
+
+    ax.set_ylim(-0.1,1.1)
+    ax.set_xlim(760,775)
+
+    if i != 2:
+        ax.get_legend().remove()
+
+    else:
+        ax.legend(['Fit', 'Fitted Data', 'Raw Data'])
+
+axes[0].set_ylabel('$\\alpha$')
+
+
+
+(ax1, ax2) = sfigs[1].subplots(nrows = 2)
 
 # Phi =0.8
-ax1 = axes[0]
+# ax1 = axes[0]
 
 ds_p_sel = ds_p.sel(phi=0.8, method='nearest').pint.dequantify()
 ds_cfd_sel = ds_cfd.sel(phi=0.8, method='nearest').pint.dequantify()
@@ -79,7 +122,7 @@ ax1.set_title('Equivalence Ratio = {}'.format(phi_val_expt))
 
 # Phi = 0.65
 
-ax2 = axes[1]
+# ax2 = axes[1]
 
 ds_p_sel = ds_p.sel(phi=0.6, method='nearest').pint.dequantify()
 ds_cfd_sel = ds_cfd.sel(phi=0.6, method='nearest').pint.dequantify()
@@ -108,60 +151,41 @@ ax2.scatter(ppu.AES_BARREL_OFFSET.to('mm').magnitude, nK_barrel_mean, color='gre
 
 ax2.set_title('Equivalence Ratio = {}'.format(phi_val_expt))
 
-for ax in axes:
-
+for ax in [ax1, ax2]:
     ax.axvline(180, color='gold', linestyle='--')
     ax.set_ylim(1e10,1e17)
     ax.set_xlim(-10,220)
     ax.set_yscale('log')
     ax.legend()
-    ax.set_ylabel('$n_K [\\#/cm^{-3}$]')
+    ax.set_ylabel('$n_K [\\mathrm{cm^{-3}}$]')
 
 ax2.set_xlabel('Stage Position [mm]')
 ax1.set_xlabel('')
-
-plt.savefig(pjoin(DIR_FIG_OUT, 'pos_nK_mwt_cfd.png'), dpi=300, bbox_inches='tight')
-
-# Phi =0.65
-# ax2 = axes[1]
-
-#%%
-
-plt.figure()
-
-ds_plot = ds_alpha_fit.sel(run=('2023-05-18', 1)).sel(phi=0.8, method='nearest')
-
-# da_plot.plot(col='phi', hue='var', row='motor')
-motor_sel = [80, 130, 180]
-
-ds_plot = ds_plot.sel(motor=motor_sel, method='nearest')
-
-fig, axes = plt.subplots(1,len(motor_sel), figsize=(5,2), sharey=True)
-
-for i, motor in enumerate(ds_plot.coords['motor']):
-    ax = axes[i]
-    ds_plot.sel(motor=motor).to_array('var').plot(hue='var', ax=ax)
-    ax.set_title('{:.0f} mm'.format(motor))
-
-    ax.set_xlabel("Wavelength [nm]")
-
-    ax.set_ylim(-0.1,1.1)
-    ax.set_xlim(760,775)
-
-    if i != 2:
-        ax.get_legend().remove()
-
-    else:
-        ax.legend(['Fit', 'Fitted Data', 'Raw Data'])
-
-axes[0].set_ylabel('$\\alpha$')
+ax1.set_xticklabels([])
 
 
-plt.savefig(pjoin(DIR_FIG_OUT, 'pos_alpha_fit.png'), dpi=300, bbox_inches='tight')
+#bounding box position changes for subfigure with multiple subplots, so the top subfigure's text does not end up in the same place as the bottom two. Right now, doing this hacky way of getting the top label to the right place. More robust would be to use figure coordinates for putting text, but that makes it way harder to put labels on multiple subplots.
+
+labels = ['B)', 'C)']
+
+axlist = [ax1, ax2]
+
+for ax, label in zip(axlist, labels):
+    ax.text(-0.15, 1.1, label, transform=ax.transAxes, va='bottom', ha='left')
+
+sfigs[0].get_axes()[0].text(-0.15, 2.75, 'A)', transform=ax1.transAxes)
+
+# plt.savefig(pjoin(DIR_FIG_OUT, 'pos_nK_mwt_cfd.png'), dpi=300, bbox_inches='tight')
+
+# # Phi =0.65
+# # ax2 = axes[1]
+
+# #%%
+# plt.savefig(pjoin(DIR_FIG_OUT, 'pos_alpha_fit.png'), dpi=300, bbox_inches='tight')
 
 #%%
 
-plt.figure()
+#Concatenate three figures into a single figure
 
 ds_plot = ds_lecroy_536.sel(run=('2023-05-18', 1)).dropna('motor', how='all')
 
@@ -170,7 +194,36 @@ da_plot = ds_plot['AS_abs']
 motor_sel = [30, 100, 180]
 motor_actual = []
 
-fig, ax = plt.subplots(figsize=(3,3))
+nrows = 3 #to scale plot height by number of rows
+
+fig, (ax1, ax2, ax3) = plt.subplots(nrows = 3)
+fig.set_figheight(nrows * 3)
+
+fig.subplots_adjust(hspace = 0.25)
+
+
+labels = ['A)', 'B)', 'C)']
+
+axlist = [ax1, ax2, ax3]
+
+for ax, label in zip(axlist, labels):
+    ax.set_title('')
+    ax.text(-0.15, 1.1, label, transform=ax.transAxes, va='top', ha='right')
+
+
+#Create two subfigures, where bottom subfigure has no vertical space between subplots
+
+
+# fig = plt.figure()
+# fig.set_figheight(nrows * 3)
+
+# sfigs = fig.subfigures(2, 1, height_ratios = [1,2], hspace = 0)
+# ax1 = sfigs[0].subplots(1, 1)
+# (ax2, ax3) = sfigs[1].subplots(2, 1, sharex = True)
+
+# sfigs[1].subplots_adjust(hspace = 0)
+
+
 
 colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']  # Define a list of colors for the traces
 
@@ -183,18 +236,17 @@ for i, motor in enumerate(motor_sel):
     motor_actual.append(motor_val)
     
     # Plot mean and shaded region for standard deviation
-    ax.plot(mean['time'].pint.magnitude, mean.pint.magnitude, color=colors[i % len(colors)], label='{:.0f} mm'.format(motor_val))
-    ax.fill_between(mean['time'].pint.magnitude, (mean-std).pint.magnitude, (mean+std).pint.magnitude, color=colors[i % len(colors)], alpha=0.2)
+    ax1.plot(mean['time'].pint.magnitude, mean.pint.magnitude, color=colors[i % len(colors)], label='{:.0f} mm'.format(motor_val))
+    ax1.fill_between(mean['time'].pint.magnitude, (mean-std).pint.magnitude, (mean+std).pint.magnitude, color=colors[i % len(colors)], alpha=0.2)
 
-ax.set_xlabel('Time [$\mu s$]')
-ax.set_ylabel('AS')
-ax.legend(bbox_to_anchor=(0.5,0.87))
-ax.set_ylim(-0.05,1.05)
+ax1.set_xlabel('Time [$\mu s$]')
+ax1.set_ylabel('AS')
+ax1.legend(bbox_to_anchor=(0.5,0.87))
+ax1.set_ylim(-0.05,1.05)
 
-plt.savefig(pjoin(DIR_FIG_OUT, 'pos_mwt_AS_time_20230518.png'), dpi=300, bbox_inches='tight')
-# %%
 
-plt.figure()
+
+# plt.savefig(pjoin(DIR_FIG_OUT, 'pos_mwt_AS_time_20230518.png'), dpi=300, bbox_inches='tight')
 
 
 da_plot = ds_lecroy_536['AS_abs'].sel(run=('2023-05-18', 1)).dropna('motor', how='all')
@@ -204,45 +256,52 @@ AS_pp_fluct = da_plot.sel(time=slice(-50,-1)).std('mnum').mean('time').pint.dequ
 AS_laser = da_plot.mwt._pulse_max().mean('mnum').pint.dequantify()
 AS_laser_fluct = da_plot.mwt._pulse_max().std('mnum').pint.dequantify()
 
-fig, ax = plt.subplots()
-
-xr_errorbar_axes(AS_pp, AS_pp_fluct, axes=ax, capsize=5, label='AS Pre Laser')
-xr_errorbar_axes(AS_laser, AS_laser_fluct, axes=ax, capsize=5, label='AS Laser')
+xr_errorbar_axes(AS_pp, AS_pp_fluct, axes=ax2, capsize=5, label='AS Pre Laser')
+xr_errorbar_axes(AS_laser, AS_laser_fluct, axes=ax2, capsize=5, label='AS Laser')
 
 for motor_val in motor_actual:
     plt.axvline(motor_val, color='k', linestyle='--', alpha=0.5)
 
-dropna(ax)
+dropna(ax2)
 
-plt.legend()
-plt.xlim(-5,270)
 
-plt.ylabel('AS')
-plt.xlabel('Stage Position [mm]')
+ax2.legend()
+ax2.set_xlim(-5,270)
+ax2.set_xlabel('Stage Position [mm]')
+ax2.set_ylabel('AS')
 
-plt.savefig(pjoin(DIR_FIG_OUT, 'pos_mwt_AS_stats_20230518.png'))
+# plt.savefig(pjoin(DIR_FIG_OUT, 'pos_mwt_AS_stats_20230518.png'))
 
-#%%
-#%%
-
-plt.figure()
 
 da_plot = ds_lecroy_536['SFR_abs'].dropna('run', how='all')
 
-g=  da_plot.mean('mnum', keep_attrs=True).plot(hue='run_plot', x='motor', marker='o')
+g =  da_plot.mean('mnum', keep_attrs=True).plot(hue='run_plot', x='motor', marker='o', ax = ax3)
 
 for motor_val in motor_actual:
-    plt.axvline(motor_val, color='k', linestyle='--', alpha=0.5)
+    ax3.axvline(motor_val, color='k', linestyle='--', alpha=0.5)
 
-plt.title('')
-plt.xlim(-5, 270)
-plt.ylim(-5,125)
-plt.xlabel('Stage Position [mm]')
-plt.ylabel('SFR')
+
+
+ax3.set_title('')
+ax3.set_xlim(-5, 270)
+ax3.set_ylim(-5,125)
+ax3.set_xlabel('Stage Position [mm]')
+ax3.set_ylabel('SFR')
 
 dropna(g)
 
-plt.savefig(pjoin(DIR_FIG_OUT, 'pow_mwt_sfr_dates.png'))
+fig.savefig('Fig4_Position_MWT.svg')
 
 
+
+
+# %%
+
+axlist = [ax1, ax2, ax3]
+
+fig_combined, axs = plt.subplots()
+
+[fig_combined.add_subplot(ax) for ax in axlist]
+
+fig_combined.show()
 # %%
