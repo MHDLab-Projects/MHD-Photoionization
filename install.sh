b1a158cf245e87b8cda5d0515e6e1945020af1a0
Lee Aspitarte <aspitarte@gmail.com>
2025-02-19 15:02:27 -0800

    rework submodule initialization in install

--- install.sh
+++ install.sh
@@ -3,18 +3,16 @@ source .env
 #TODO: setup.py needs to be replaced with pyproject.toml
 python setup.py develop # For pi_paper_utils package
 
-git submodule update --init
+git submodule update --init --recursive mhdlab
 
 pushd mhdlab
 pip install -e .
 popd
 
-pushd mhdlab/mhdlab/fileio
-git submodule update --init
-popd
-
 if [ "$MUNGE_DATA" = true ] ; then
     # Only post process if we need to munge the data, not currently releasing post processing with paper
+
+    git submodule update --init PostProcessor
 
     pushd PostProcessor
     # This dummy directory is needed to get test discovery to work
