8a41608764499f814e2fe9e96215785866476b55
Lee Aspitarte <aspitarte@gmail.com>
2025-02-21 15:50:14 -0800

    work on 2023-05-18 movie
    add edited figures
    tweak iccd movie

--- experiment/movies/gen_movie_iccd.py
+++ experiment/movies/gen_movie_iccd.py
@@ -42,24 +42,30 @@ def gen_movie_iccd(da_sel_tf, fp_out, duration):
 
     def make_frame(t):
 
-        fig, ax = plt.subplots(figsize=(6, 6))
+        fig, ax = plt.subplots(figsize=(6, 4))
         nearest_time = ts[int(t * fps)]
         da_sel = da_sel_tf.sel(gatedelay=nearest_time, method='nearest')
 
-        da_sel.plot(ax=ax, vmin=0, vmax=500)
-        ax.set_title(f'Gate Delay: {nearest_time} ns')
+        da_sel.plot(ax=ax, x='x', y='y', vmin=0, vmax=500)
+        ax.set_title(f'Optical Emission\nGate Delay: {nearest_time} ns')
+
+        ax.invert_xaxis()
 
         ax.set_xlabel('x (mm)')
         ax.set_ylabel('y (mm)')
 
         fig.tight_layout()
 
-        return mplfig_to_npimage(fig)
+        np_out = mplfig_to_npimage(fig)
+
+        plt.close(fig)
+
+        return np_out
 
     animation = VideoClip(make_frame, duration=duration)
     animation.write_videofile(fp_out, fps=fps)
 
 # Example usage
 fp_out = pjoin('output', 'iccd_movie_2025_05_24.mp4')
-gen_movie_iccd(da_sel_tf, fp_out, duration=5)
+gen_movie_iccd(da_sel_tf, fp_out, duration=2.5)
 # %%
